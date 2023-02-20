440b71a22f14ff786310711431888847da5224e9
Fauzi Nur Noviansyah <fnnoviansyah@gmail.com>
2023-02-20 14:34:33 +0700

    implement feedbacks

---
+++ watch_list/test/domain/use_cases/get_tv_series_watch_list_test.dart
@@ -0,0 +1,30 @@
+import 'package:dartz/dartz.dart';
+import 'package:ditonton/domain/usecases/get_watchlist_tv_series.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+
+import '../../../../test/dummy_data/dummy_objects.dart';
+import '../../../../test/helpers/test_helper.mocks.dart';
+
+void main() {
+  late GetWatchlistTvSeries usecase;
+  late MockTvSeriesRepository mockTvSeriesRepository;
+
+  setUp(() {
+    mockTvSeriesRepository = MockTvSeriesRepository();
+    usecase = GetWatchlistTvSeries(mockTvSeriesRepository);
+  });
+
+  test('should get list of tv series from the repository', () async {
+    // arrange
+    when(
+      mockTvSeriesRepository.getWatchlistTvSeries(),
+    ).thenAnswer(
+      (_) async => Right(testTvSeriesList),
+    );
+    // act
+    final result = await usecase.execute();
+    // assert
+    expect(result, Right(testTvSeriesList));
+  });
+}
