2dadfe8eeaf8ad24ea2af4c738fcf306dd9504a7
Fauzi Nur Noviansyah <fnnoviansyah@gmail.com>
2023-02-20 07:44:22 +0700

    upgrade flutter version

--- main/lib/features/home_movie/presentation/pages/app_home_movie_page.dart
+++ main/lib/features/home_movie/presentation/pages/app_home_movie_page.dart
@@ -1,5 +1,4 @@
 import 'package:core/domain/entities/movie.dart';
-import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:movie/presentation/pages/movie_detail_page.dart';
 import 'package:movie/presentation/pages/movie_page.dart';
